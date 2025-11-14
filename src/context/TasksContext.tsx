0129ab31f828c13d9e1630a42cddee93e0030260
Niranjan <niranjancb2035@gmail.com>
2025-11-14 15:08:24 +0530

    fix : vercel error

--- src/context/TasksContext.tsx
+++ src/context/TasksContext.tsx
@@ -9,7 +9,8 @@ interface TasksContextValue {
   derivedSorted: DerivedTask[];
   metrics: Metrics;
   lastDeleted: Task | null;
-  addTask: (task: Omit<Task, 'id'> & { id?: string }) => void;
+ addTask: (task: Partial<Task>) => void;
+
   updateTask: (id: string, patch: Partial<Task>) => void;
   deleteTask: (id: string) => void;
   undoDelete: () => void;
